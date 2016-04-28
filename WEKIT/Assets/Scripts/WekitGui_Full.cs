77203a5de302871ce6c5f60d125792e2eedc3ac8
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-04-28 16:12:26 +0200

    Adjustments

--- WEKIT/Assets/Scripts/WekitGui_Full.cs
+++ WEKIT/Assets/Scripts/WekitGui_Full.cs
@@ -18,76 +18,74 @@ public class WekitGui_Full : WekitGui
     public override void OnGUI()
     {
         base.OnGUI();
-        if (ShowOptions)
+        if (!ShowOptions) return;
+        if (!Player.Recording)
         {
-            if (!Player.Recording)
+            if (!Player.Replaying)
             {
-                if (!Player.Replaying)
-                {
-                    //Record button
-                    if (GUI.Button(new Rect(StandardWidth,0,StandardWidth,StandardHeight), "Record"+ (KeyInput != null ? KeyToText(KeyInput.RecordKey) : "")))
-                    {
-                        _currentTime = Player.CountDown;
-                        Player.Record();
-                    }
-                    Player.CountDown = Single.Parse(GUI.TextField(new Rect(StandardWidth, StandardHeight, StandardWidth, StandardHeight), Player.CountDown.ToString(), 25));
-                    Player.Stepsize = (int)GUI.HorizontalSlider(new Rect(StandardWidth, StandardHeight*2, StandardWidth, StandardHeight), Player.Stepsize, 1, 3);
-
-                    //Load button
-                    if (GUI.Button(new Rect(StandardWidth*3, 0, StandardWidth, StandardHeight), "Load"))
-                    {
-                        Load();
-                    }
-
-                    Player.LoadFileName = GUI.TextField(new Rect(StandardWidth*3, StandardHeight, StandardWidth, StandardHeight), Player.LoadFileName, 25);
-                }
-
-                //Save button
-                if (GUI.Button(new Rect(StandardWidth*4, 0, StandardWidth, StandardHeight), "Save"))
-                {
-                    Player.Save();
-                    if (UseXml)
-                    {
-                        XMLData data = new XMLData(new XMLFileInfo(Player.UseCompoundArchive ? Player.CompoundZipName : Player.FileName, Player.FileName, Player.UseZip));
-
-                        if (!Player.UseZip)
-                        {
-                            XmlSerializer serializer = new XmlSerializer(typeof(XMLData));
-                            FileStream file = File.Open(Player.SavePath + "/" + Player.CustomDirectory + "/" + Player.FileName + ".txt", FileMode.OpenOrCreate);
-                            serializer.Serialize(file, data);
-                            file.Close();
-                        }
-                        else
-                        {
-                            string filestring = Player.SavePath + "/" + Player.CustomDirectory + "/" + (Player.UseCompoundArchive ? Player.CompoundZipName : Player.FileName) + ".zip";
-                            Compression.AddItemToCompoundArchive(filestring, Player.FileName + ".txt", ref data, new XmlSerializer(typeof(XMLData)));
-                        } 
-                    }
-                }
-                Player.FileName = GUI.TextField(new Rect(StandardWidth*4, StandardHeight, StandardWidth, StandardHeight), Player.FileName, 25);
-
-                //Delete button
-                if (GUI.Button(new Rect(StandardWidth*5, 0, StandardWidth, StandardHeight), "Delete"))
-                {
-                    Player.Delete();
-                }
-                Player.DeleteFileName = GUI.TextField(new Rect(StandardWidth*5, StandardHeight, StandardWidth, StandardHeight), Player.DeleteFileName, 25);
-
-            }
-
-            //If recording
-            else
-            {
-                //Record (stop) button
-                if (GUI.Button(new Rect(StandardWidth, 0, StandardWidth, StandardHeight), "Stop"+ (KeyInput != null ? KeyToText(KeyInput.RecordKey) : "")))
+                //Record button
+                if (GUI.Button(new Rect(StandardWidth,0,StandardWidth,StandardHeight), "Record"+ (KeyInput != null ? KeyToText(KeyInput.RecordKey) : "")))
                 {
                     _currentTime = Player.CountDown;
                     Player.Record();
                 }
-                if (_currentTime > 0)
+                Player.CountDown = Single.Parse(GUI.TextField(new Rect(StandardWidth, StandardHeight, StandardWidth, StandardHeight), Player.CountDown.ToString(), 25));
+                Player.Stepsize = (int)GUI.HorizontalSlider(new Rect(StandardWidth, StandardHeight*2, StandardWidth, StandardHeight), Player.Stepsize, 1, 3);
+
+                //Load button
+                if (GUI.Button(new Rect(StandardWidth*3, 0, StandardWidth, StandardHeight), "Load"))
                 {
-                    GUI.Label(new Rect(StandardWidth, StandardHeight, StandardWidth, StandardHeight), _currentTime.ToString(), new GUIStyle());
+                    Load();
                 }
+
+                Player.LoadFileName = GUI.TextField(new Rect(StandardWidth*3, StandardHeight, StandardWidth, StandardHeight), Player.LoadFileName, 25);
+            }
+
+            //Save button
+            if (GUI.Button(new Rect(StandardWidth*4, 0, StandardWidth, StandardHeight), "Save"))
+            {
+                Player.Save();
+                if (UseXml)
+                {
+                    XMLData data = new XMLData(new XMLFileInfo(Player.UseCompoundArchive ? Player.CompoundZipName : Player.FileName, Player.FileName, Player.UseZip));
+
+                    if (!Player.UseZip)
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(XMLData));
+                        FileStream file = File.Open(Player.SavePath + "/" + Player.CustomDirectory + "/" + Player.FileName + ".txt", FileMode.OpenOrCreate);
+                        serializer.Serialize(file, data);
+                        file.Close();
+                    }
+                    else
+                    {
+                        string filestring = Player.SavePath + "/" + Player.CustomDirectory + "/" + (Player.UseCompoundArchive ? Player.CompoundZipName : Player.FileName) + ".zip";
+                        Compression.AddItemToCompoundArchive(filestring, Player.FileName + ".txt", ref data, new XmlSerializer(typeof(XMLData)));
+                    } 
+                }
+            }
+            Player.FileName = GUI.TextField(new Rect(StandardWidth*4, StandardHeight, StandardWidth, StandardHeight), Player.FileName, 25);
+
+            //Delete button
+            if (GUI.Button(new Rect(StandardWidth*5, 0, StandardWidth, StandardHeight), "Delete"))
+            {
+                Player.Delete();
+            }
+            Player.DeleteFileName = GUI.TextField(new Rect(StandardWidth*5, StandardHeight, StandardWidth, StandardHeight), Player.DeleteFileName, 25);
+
+        }
+
+        //If recording
+        else
+        {
+            //Record (stop) button
+            if (GUI.Button(new Rect(StandardWidth, 0, StandardWidth, StandardHeight), "Stop"+ (KeyInput != null ? KeyToText(KeyInput.RecordKey) : "")))
+            {
+                _currentTime = Player.CountDown;
+                Player.Record();
+            }
+            if (_currentTime > 0)
+            {
+                GUI.Label(new Rect(StandardWidth, StandardHeight, StandardWidth, StandardHeight), _currentTime.ToString(), new GUIStyle());
             }
         }
     }
--- WEKIT/Assets/Scripts/WekitGui_Replay.cs
+++ WEKIT/Assets/Scripts/WekitGui_Replay.cs
@@ -8,15 +8,16 @@ public class WekitGui_Replay : WekitGui
         base.OnGUI();
 
         if (!ShowOptions || Player.Replaying) return;
-        
+
+
+        Player.LoadFileName =
+            GUI.TextField(new Rect(StandardWidth * 3, StandardHeight, StandardWidth, StandardHeight),
+                Player.LoadFileName, 25);
+
         //Load button
         if (GUI.Button(new Rect(StandardWidth*3, 0, StandardWidth, StandardHeight), "Load"))
         {
             Load();
         }
-
-        Player.LoadFileName =
-            GUI.TextField(new Rect(StandardWidth*3, StandardHeight, StandardWidth, StandardHeight),
-                Player.LoadFileName, 25);
     }
 }
