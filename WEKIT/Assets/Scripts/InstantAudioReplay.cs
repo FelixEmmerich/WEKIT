cc749a21fe3c3c4b87f4f5550dc9da47bec59cca
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-05-19 11:53:26 +0200

    Created demo replay. Fixed Leap Quaternions not being serializable. Added script that streams audio directly for test setup. Removed unnecessary gameobjects from Wekit and Wekit Ghost scenes.

---
+++ WEKIT/Assets/Scripts/InstantAudioReplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantAudioReplay : MonoBehaviour
+{
+    public AudioSource Audio;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Audio.clip = Microphone.Start("", true, 100, 44100);
+        Audio.loop = true;
+        while (!(Microphone.GetPosition("") > 0)) { }
+        Audio.Play();
+    }
+}
