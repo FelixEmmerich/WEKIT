e35025b9e8ea06e928964e40bfdef82dde415937
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-06-14 15:34:05 +0200

    Networking (Safety save)

--- WEKIT/Assets/Scripts/NetworkTest.cs
+++ WEKIT/Assets/Scripts/NetworkTest.cs
@@ -8,13 +8,39 @@ public class NetworkTest : NetworkBehaviour
         if (!isLocalPlayer)
             return;
 
-        var x = Input.GetAxis("Horizontal") * 0.1f;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CmdTest("Original");
+        }
+
+        /*var x = Input.GetAxis("Horizontal") * 0.1f;
         var y = Input.GetAxis("Vertical") * 0.1f;
 
-        transform.Translate(x, y, 0);
+        transform.Translate(x, y, 0);*/
     }
+
     public override void OnStartLocalPlayer()
     {
         GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    [Command]
+    void CmdTest(string muhstring)
+    {
+        Debug.Log("Server: " + muhstring);
+        RpcTest(muhstring + " modified");
+    }
+
+    [ClientRpc]
+    void RpcTest(string muhstring)
+    {
+        if (!this.hasAuthority)
+        {
+            Debug.Log("Client: " + muhstring);
+        }
+        else
+        {
+            Debug.Log("Authority");
+        }
     }
 }
