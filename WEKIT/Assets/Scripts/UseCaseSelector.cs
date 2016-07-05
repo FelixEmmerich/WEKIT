827a5dd3576d831afa1b17f21dc00daacbc9d7e0
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-07-05 11:41:46 +0200

    Began work on use case selection.

---
+++ WEKIT/Assets/Scripts/UseCaseSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class UseCaseSelector : MonoBehaviour
+{
+
+	[Serializable]
+	public class UseCase
+	{
+		public List<UseCaseElement> Elements;
+	}
+
+	[Serializable]
+	public class UseCaseElement
+	{
+		public WekitPlayer_Base Player;
+		public string Text;
+	}
+
+	public List<UseCase> UseCases;
+	public int CurrentIndex=-1;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+}
