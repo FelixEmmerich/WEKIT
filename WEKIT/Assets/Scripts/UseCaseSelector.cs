f4e40b18e31796ce7447b2d0a7bf447fa13a8977
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-07-18 15:26:16 +0200

    Use Cases

--- WEKIT/Assets/Scripts/UseCaseSelector.cs
+++ WEKIT/Assets/Scripts/UseCaseSelector.cs
@@ -8,8 +8,8 @@ public class UseCaseSelector : MonoBehaviour
 	[Serializable]
 	public class UseCase
 	{
-		public List<UseCaseElement> Elements;
 		public string Name;
+		public List<UseCaseElement> Elements;
 	}
 
 	[Serializable]
@@ -24,6 +24,7 @@ public class UseCaseSelector : MonoBehaviour
 	public int UseCaseElementIndex = 0;
 	public WekitPlayerContainer Container;
 	public float ButtonSize = 20f;
+	private bool _disable = false;
 
 	public virtual void OnGUI()
 	{
@@ -32,7 +33,7 @@ public class UseCaseSelector : MonoBehaviour
 			for (int i = 0; i < UseCases.Count; i++)
 			{
 				UseCase useCase = UseCases[i];
-				if (!GUI.Button(new Rect(0, i*ButtonSize, ButtonSize, ButtonSize), UseCases[i].Name))
+				if (GUI.Button(new Rect(0, i*ButtonSize, ButtonSize, ButtonSize), UseCases[i].Name))
 				{
 					UseCaseIndex = i;
 				}
@@ -40,7 +41,36 @@ public class UseCaseSelector : MonoBehaviour
 		}
 		else
 		{
+			if (UseCaseElementIndex < UseCases[UseCaseIndex].Elements.Count)
+			{
+				if (GUI.Button(new Rect(0, 0, ButtonSize, ButtonSize),
+					UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Text))
+				{
+					if (UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Player != null)
+					{
+						WekitPlayer_Base player = UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Player;
+						player.Enabled(true);
+						Container.ActiveWekitPlayers.Add(player);
+						Container.WekitPlayers.Add(player);
+					}
+					UseCaseElementIndex++;
 
+				}
+			}
+			else
+			{
+				Container.Enabled(true);
+				_disable = true;
+			}
+		}
+	}
+
+	//Disabling the gameobject inside OnGUI causes a Unity error, so this function is called instead
+	void Update()
+	{
+		if (_disable)
+		{
+			transform.root.gameObject.SetActive(false); 
 		}
 	}
 
