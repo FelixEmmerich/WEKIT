7acb3befc4bcb08dbb9434d5d0059288bcec28b8
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-07-19 15:20:00 +0200

    Began work on XML integration for use cases

--- WEKIT/Assets/Scripts/UseCaseSelector.cs
+++ WEKIT/Assets/Scripts/UseCaseSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 public class UseCaseSelector : MonoBehaviour
 {
@@ -8,50 +9,62 @@ public class UseCaseSelector : MonoBehaviour
 	[Serializable]
 	public class UseCase
 	{
-		public string Name;
-		public List<UseCaseElement> Elements;
+		public string UseCaseName;
+		public UseCaseElement[] Elements;
 	}
 
 	[Serializable]
 	public class UseCaseElement
 	{
-		public WekitPlayer_Base Player;
-		public string Text;
+		public string PlayerName;
+		public string Message;
 	}
 
-	public List<UseCase> UseCases;
+	public UseCase[] UseCases;
 	public int UseCaseIndex=-1;
-	public int UseCaseElementIndex = 0;
+	public int UseCaseElementIndex;
 	public WekitPlayerContainer Container;
 	public float ButtonSize = 20f;
 	private bool _disable = false;
+	public WekitPlayer_Base[] Players;
+
+	void Start()
+	{
+		//Players = Resources.FindObjectsOfTypeAll(typeof(WekitPlayer_Base)) as WekitPlayer_Base[];
+	}
 
 	public virtual void OnGUI()
 	{
-		if (UseCaseIndex < 0)
+		if (UseCaseIndex<0)
 		{
-			for (int i = 0; i < UseCases.Count; i++)
+			if (UseCases.Length>0)
 			{
-				UseCase useCase = UseCases[i];
-				if (GUI.Button(new Rect(0, i*ButtonSize, ButtonSize, ButtonSize), UseCases[i].Name))
+				float height = Screen.height / (float)UseCases.Length;
+				for (int i = 0; i < UseCases.Length; i++)
 				{
-					UseCaseIndex = i;
-				}
+					if (GUI.Button(new Rect(0, i * height, Screen.width, height), UseCases[i].UseCaseName))
+					{
+						UseCaseIndex = i;
+					}
+				} 
 			}
 		}
 		else
 		{
-			if (UseCaseElementIndex < UseCases[UseCaseIndex].Elements.Count)
+			if (UseCaseElementIndex < UseCases[UseCaseIndex].Elements.Length)
 			{
-				if (GUI.Button(new Rect(0, 0, ButtonSize, ButtonSize),
-					UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Text))
+				if (GUI.Button(new Rect(0, 0, Screen.width, Screen.height),
+					UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Message))
 				{
-					if (UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Player != null)
+					foreach (WekitPlayer_Base entry in Players)
 					{
-						WekitPlayer_Base player = UseCases[UseCaseIndex].Elements[UseCaseElementIndex].Player;
-						player.Enabled(true);
-						Container.ActiveWekitPlayers.Add(player);
-						Container.WekitPlayers.Add(player);
+						if (entry.PlayerName == UseCases[UseCaseIndex].Elements[UseCaseElementIndex].PlayerName)
+						{
+							entry.Enabled(true);
+							Container.ActiveWekitPlayers.Add(entry);
+							Container.WekitPlayers.Add(entry);
+							break;
+						}
 					}
 					UseCaseElementIndex++;
 
@@ -71,6 +84,25 @@ public class UseCaseSelector : MonoBehaviour
 		if (_disable)
 		{
 			transform.root.gameObject.SetActive(false); 
+		}
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			CreateExampleXml("");
+		}
+	}
+
+	//Creates an example XML text file containing the names of all players (except container (this assumes only one container in the scene)) in case the original gets deleted or broken
+	void CreateExampleXml(string name)
+	{
+		UseCases=new UseCase[Players.Length];
+		for (int i = 0; i < Players.Length; i++)
+		{
+			UseCases[i]=new UseCase();
+			UseCases[i].UseCaseName = "Element" + i;
+			UseCases[i].Elements = new UseCaseElement[1];
+			UseCases[i].Elements[0]=new UseCaseElement();
+			UseCases[i].Elements[0].PlayerName = Players[i].PlayerName;
+			UseCases[i].Elements[0].Message = "Connect "+Players[i].PlayerName;
 		}
 	}
 
